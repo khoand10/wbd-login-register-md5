683449dbfbda5fd1f306976554ff929da599e020
dangkhoa <khoand1011@gmail.com>
2018-05-15 17:12:30 +0700

    push all code

--- src/com/codegym/service/UserService.java
+++ src/com/codegym/service/UserService.java
@@ -1,6 +1,9 @@
 package com.codegym.service;
 
 import com.codegym.model.User;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -10,15 +13,17 @@ public class UserService {
         String username = user.getUsername(); //get username value through loginBean object and store in temporary variable "username"
         String password = user.getPassword(); //get password value through loginBean object and store in temporary variable "password"
 
+        password = encodeMD5(password);
+
         String dbusername = "";  //create two variable for use next process
         String dbpassword = "";
 
         try {
-            Connection con = ConnectionUtil.getConnection("localhost", "root", "123456", "mydb", "3306");
+            Connection con = ConnectionUtil.getConnection("localhost", "root", "dangkhoa1997", "mydb", "3306");
 
             PreparedStatement pstmt = null; //create statement
 
-            pstmt = con.prepareStatement("select * from user where username=? and password=?"); //sql select query
+            pstmt = con.prepareStatement("select * from users where username=? and password=?"); //sql select query
             pstmt.setString(1, username);
             pstmt.setString(2, password);
             ResultSet rs = pstmt.executeQuery(); //execute query and set in Resultset object rs.
@@ -51,11 +56,11 @@ public class UserService {
         String password = user.getPassword();
 
         try {
-            Connection con = ConnectionUtil.getConnection("localhost", "root", "123456", "mydb", "3306");
+            Connection con = ConnectionUtil.getConnection("localhost", "root", "dangkhoa1997", "mydb", "3306");
 
             PreparedStatement pstmt = null; //create statement
 
-            pstmt = con.prepareStatement("insert into user(firstname,lastname,username,password) values(?,?,?,md5(?))"); //sql insert query
+            pstmt = con.prepareStatement("insert into users(firstname,lastname,username,password) values(?,?,?,md5(?))"); //sql insert query
             pstmt.setString(1, firstname);
             pstmt.setString(2, lastname);
             pstmt.setString(3, username);
@@ -69,5 +74,23 @@ public class UserService {
             e.printStackTrace();
         }
         return "FAIL REGISTER"; //if invalid return string "FAIL REGISTER"
+    }
+
+    public String encodeMD5(String password) {
+        StringBuffer sb = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(password.getBytes());
+
+            byte byteData[] = md.digest();
+
+            sb = new StringBuffer();
+            for (int i = 0; i < byteData.length; i++) {
+                sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+            }
+        } catch(NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
     }
 }
